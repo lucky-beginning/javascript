eab8caacbe099951b4c39e62f7aa7473557dec4d
lucky-beginning <luckykumar1617032@gmail.com>
2025-04-26 05:38:13 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS cose as newer version
+
+//alert(3 + 3)// we are using nodejs, not browser
+
+console.log(3 
+    +
+     3); // code readablity should be high.
+
+console.log("Lucky");
+
+let name = "Lucky";
+let age = 20;
+let isLoggedIn = true;
+
+//console.log(typeof isLoggedIn);
+
+console.log(typeof undefined);
+console.log(typeof null);
+
