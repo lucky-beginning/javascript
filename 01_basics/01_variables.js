31b3e8f84438f7b9185ac89364ff1b3f41ee82fb
lucky-beginning <luckykumar1617032@gmail.com>
2025-04-26 05:21:50 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+let name = "Lucky";
+var age = 20;
+let height = 5.8;
+state = "Delhi";
+let bG;
+
+ height = 6;
+ age = 21;
+ state = "Nepal";
+ name = "kumar";
+
+console.table([height,age,state,name,bG]);
+
+
