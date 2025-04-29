016bc06218ed01e9abbc2c9cbf9acb92cf1690d3
lucky-beginning <luckykumar1617032@gmail.com>
2025-04-29 09:44:12 +0000

    Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,48 @@
+const name = "lucky";
+const rollNumber = 7;
+
+// Old method console.log(name +  rollNumber);
+
+//console.log(`Hello my name is ${name} and my roll number is ${rollNumber} Thank you`);
+
+const myName = new String('lucky-webDev')
+// console.log(myName);
+
+// console.log(myName[0]);
+// console.log(myName.__proto__)
+
+// console.log(myName.length);
+// console.log(myName.toUpperCase())
+
+// console.log(myName.charAt(5));
+// console.log(myName.indexOf('b'));
+
+const newString = myName.substring(0,4);
+// console.log(newString);
+
+
+const anotherString = myName.slice(2,8);
+// console.log(anotherString); *****DOUGHT***
+
+const trimString = "      maharishi      "
+// console.log(trimString);
+// console.log(trimString.trim());
+
+const url = "https///@lucky....%20end"
+
+console.log(url.replace('lucky', 'new'));
+console.log(url.includes('....'));
+console.log(url.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
