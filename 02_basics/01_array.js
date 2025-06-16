6cfd9bc620504070f2a412b45143f47cf56473d3
lucky-beginning <luckykumar1617032@gmail.com>
2025-06-16 04:49:47 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,39 @@
+let arr = ["superman", "batman", "flash"]
+//console.log(arr);
+//console.log(arr[1]);
+
+/**************Array Moths***********************/
+
+arr.push("Aquaman");
+//arr.pop()
+//console.log(arr);
+
+arr.unshift("WonderWoman");
+//arr.shift();
+//console.log(arr);
+
+//console.log(arr.includes("flash"));
+//console.log(arr.indexOf("flash"));
+
+//const anotherArr = arr.join()
+//console.log(arr);
+//console.log(anotherArr);
+
+console.log("A",arr);
+
+const firstArr = arr.slice(3,5);
+console.log(firstArr);
+console.log("B",arr);
+
+const secArr = arr.splice(3,5);
+console.log(secArr);
+console.log("C",arr);
+
+
+
+
+
+
+
+
+
